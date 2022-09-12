2709f46c39e8a78ea9446a79b69bd75bb6c4ab1d
Marc Tofan <tofmana@gmail.com>
2022-09-12 20:55:39 +0200

    strict mode commented in

--- src/main.jsx
+++ src/main.jsx
@@ -2,16 +2,16 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
-// My imports
+// EXTRA
 import store from "./store/store";
 import App from "./App";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  // <React.StrictMode>
+  <React.StrictMode>
     <Provider store={store}>
       <BrowserRouter>
         <App />
       </BrowserRouter>
     </Provider>
-  // {/* </React.StrictMode> */}
+  </React.StrictMode>
 );
