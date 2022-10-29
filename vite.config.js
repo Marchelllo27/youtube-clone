ddbb714a07904fab5a721193fdbbdeeebbf5c7ca
Marc Tofan <tofmana@gmail.com>
2022-10-29 16:10:05 +0200

    vitest installed, testing environment added

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,13 @@
+/// <reference types="vitest" />
+
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env": process.env,
+  test: {
+    globals: true,
+    environment: "jsdom",
   },
 });
